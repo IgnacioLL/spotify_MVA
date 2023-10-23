a3232a8a579f7cf44f5f277b115b5ed18b225f61
Marc Fortó <mforto14@hotmail.com>
2023-10-23 12:12:16 +0200

    PCA with qualitative variables

--- scripts/PCA.R
+++ scripts/PCA.R
@@ -80,3 +80,137 @@ axis(side=4, pos= 0, labels = F)
 arrows(ze, ze, X, Y, length = 0.07,col="blue")
 text(X,Y,labels=etiq,col="darkblue", cex=0.7)
 
+#Qualitative
+
+# PROJECTION OF ILLUSTRATIVE qualitative variables on individuals' map
+# PROJECCI? OF INDIVIDUALS DIFFERENTIATING THE Dictamen
+# (we need a numeric Dictamen to color)
+df_wk_i$Album_type <- as.factor(df_wk_i$Album_type)
+
+varcat=factor(df_wk_i[,29])
+plot(Psi[,1],Psi[,2])
+plot(Psi[,1],Psi[,2],col=varcat)
+axis(side=1, pos= 0, labels = F, col="darkgray")
+axis(side=3, pos= 0, labels = F, col="darkgray")
+axis(side=2, pos= 0, labels = F, col="darkgray")
+axis(side=4, pos= 0, labels = F, col="darkgray")
+legend("bottomleft",levels(factor(varcat)),pch=1,col=c(1,2), cex=0.6)
+
+#select your qualitative variable
+k<-6 #dictamen in credsco
+
+varcat<-factor(df_wk_i[,k])
+fdic1 = tapply(Psi[,eje1],varcat,mean)#centroid
+fdic2 = tapply(Psi[,eje2],varcat,mean) 
+#points(fdic1,fdic2,pch=16,col="blue", labels=levels(varcat))
+text(fdic1,fdic2,labels=levels(varcat),col="yellow", cex=0.7)
+
+#if we have many variables, we can split every variables with many variables, but when we represent centroids, we represent all the numerical and categorical variables
+
+
+#Now we project both cdgs of levels of a selected qualitative variable without
+#representing the individual anymore
+
+plot(Psi[,eje1],Psi[,eje2],type="n")
+axis(side=1, pos= 0, labels = F, col="cyan")
+axis(side=3, pos= 0, labels = F, col="cyan")
+axis(side=2, pos= 0, labels = F, col="cyan")
+axis(side=4, pos= 0, labels = F, col="cyan")
+
+#select your qualitative variable
+k<-6 #dictamen in credsco
+
+#varcat<-df_wk_i[,k]
+#fdic1 = tapply(Psi[,eje1],varcat,mean)
+#fdic2 = tapply(Psi[,eje2],varcat,mean) 
+
+#points(fdic1,fdic2,pch=16,col="blue", labels=levels(varcat))
+text(fdic1,fdic2,labels=levels(varcat),col="blue", cex=0.7)
+
+
+#all qualitative together
+plot(Psi[,eje1],Psi[,eje2],type="n")
+axis(side=1, pos= 0, labels = F, col="cyan")
+axis(side=3, pos= 0, labels = F, col="cyan")
+axis(side=2, pos= 0, labels = F, col="cyan")
+axis(side=4, pos= 0, labels = F, col="cyan")
+
+#nominal qualitative variables
+
+dcat<-c(6,9,21,22)
+#divide categoricals in several graphs if joint representation saturates
+
+#build a palette with as much colors as qualitative variables 
+
+#colors<-c("blue","red","green","orange","darkgreen")
+#alternative
+colors<-rainbow(length(dcat))
+
+c<-1
+for(k in dcat){
+  seguentColor<-colors[c]
+  fdic1 = tapply(Psi[,eje1],df_wk_i[,k],mean)
+  fdic2 = tapply(Psi[,eje2],df_wk_i[,k],mean) 
+  
+  text(fdic1,fdic2,labels=levels(factor(df_wk_i[,k])),col=seguentColor, cex=0.6)
+  c<-c+1
+}
+legend("bottomleft",names(df_wk_i)[dcat],pch=1,col=colors, cex=0.6)
+
+#determine zoom level
+#use the scale factor or not depending on the position of centroids
+# ES UN FACTOR D'ESCALA PER DIBUIXAR LES FLETXES MES VISIBLES EN EL GRAFIC
+fm = round(max(abs(Psi[,1]))) 
+fm=20
+
+#scale the projected variables
+#X<-fm*U[,eje1]
+#Y<-fm*U[,eje2]
+
+#represent numerical variables in background
+plot(Psi[,eje1],Psi[,eje2],type="n",xlim=c(-1,1), ylim=c(-3,1))
+#plot(X,Y,type="none",xlim=c(min(X,0),max(X,0)))
+axis(side=1, pos= 0, labels = F, col="cyan")
+axis(side=3, pos= 0, labels = F, col="cyan")
+axis(side=2, pos= 0, labels = F, col="cyan")
+axis(side=4, pos= 0, labels = F, col="cyan")
+
+#adf_wk_i projections of numerical variables in background
+arrows(ze, ze, X, Y, length = 0.07,col="lightgray")
+text(X,Y,labels=etiq,col="gray", cex=0.7)
+
+#adf_wk_i centroids
+c<-1
+for(k in dcat){
+  seguentColor<-colors[c]
+  
+  fdic1 = tapply(Psi[,eje1],df_wk_i[,k],mean)
+  fdic2 = tapply(Psi[,eje2],df_wk_i[,k],mean) 
+  
+  #points(fdic1,fdic2,pch=16,col=seguentColor, labels=levels(df_wk_i[,k]))
+  text(fdic1,fdic2,labels=levels(factor(df_wk_i[,k])),col=seguentColor, cex=0.6)
+  c<-c+1
+}
+legend("bottomleft",names(df_wk_i)[dcat],pch=1,col=colors, cex=0.6)
+
+
+#add ordinal variables
+dordi<-c(28,29)
+
+c<-1
+col<-1
+for(k in dordi){
+  seguentColor<-colors[col]
+  fdic1 = tapply(Psi[,eje1],df_wk_i[,k],mean)
+  fdic2 = tapply(Psi[,eje2],df_wk_i[,k],mean) 
+  
+  #points(fdic1,fdic2,pch=16,col=seguentColor, labels=levels(df_wk_i[,k]))
+  #connect modalities of qualitative variables
+  lines(fdic1,fdic2,pch=16,col=seguentColor)
+  text(fdic1,fdic2,labels=levels(df_wk_i[,k]),col=seguentColor, cex=0.6)
+  c<-c+1
+  col<-col+1
+}
+legend("topleft",names(df_wk_i)[dordi],pch=1,col=colors[1:length(dordi)], cex=0.6)
+
+
