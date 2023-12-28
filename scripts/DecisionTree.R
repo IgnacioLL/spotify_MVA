eebc8472528639015fd649ed2e0db3d4012e9c20
inigopm <inigopm@gmail.com>
2023-12-29 00:19:24 +0100

    Checkpoint CART (not finished yet)

--- scripts/DecisionTree.R
+++ scripts/DecisionTree.R
@@ -2,8 +2,9 @@
 
 library(rpart)
 library(rpart.plot)
-df_wk_i <- readRDS("preprocessing.Rdata") #el que tiene el merge
-
+library(pROC)
+setwd("C:/Users/usuario/Desktop/MVA/spotify_MVA/scripts")
+df_wk_i <- readRDS("../report/preprocessing.Rdata") #el que tiene el merge
 
 dim(df_wk_i)
 
@@ -43,7 +44,6 @@ dtot <- data.frame(Album_type,Danceability,Energy,Key,Loudness,Speechiness,Acous
 n <- nrow(dtot)
 learn <- sample(1:n, round(0.67*n))
 head(learn)
-
 nlearn <- length(learn)
 nlearn
 ntest <- n - nlearn
@@ -123,63 +123,120 @@ dim(dtot)
 nlearn
 dim(dtot[learn,])
 
-table(Dictamen,exclude=FALSE)
-table(df_wk_i[learn,1])
+table(genre,exclude=FALSE)
+table(df_wk_i[learn,15])
 
 
-
-table(df_wk_i[,1])
+table(df_wk_i[,15])
 
 
 # CACULATE THE ERROR RATE IN THE LEARNING SAMPLE
-
 l<-0.5
-l<-0.2
+l<-0.3
 predClass=NULL
-predClass[predictions[,1]<l]="pred_neg.learn"
-predClass[predictions[,1]>=l]="pred_pos.learn"
+predClass[predictions[,1]>=l]="pred_DarkTrap"
+predClass[predictions[,2]>=l]="pred_dnb"
+predClass[predictions[,3]>=l]="pred_Emo"
+predClass[predictions[,4]>=l]="pred_hardstyle"
+predClass[predictions[,5]>=l]="pred_Hiphop"
+predClass[predictions[,6]>=l]="pred_Pop"
+predClass[predictions[,7]>=l]="pred_psytrance"
+predClass[predictions[,8]>=l]="pred_Rap"
+predClass[predictions[,9]>=l]="pred_RnB"
+predClass[predictions[,10]>=l]="pred_techhouse"
+predClass[predictions[,11]>=l]="pred_trance"
+predClass[predictions[,12]>=l]="pred_trap"
+predClass[predictions[,13]>=l]="pred_Trap_Metal"
+predClass[predictions[,14]>=l]="pred_Underground_Rap"
 predictions[1:5]
 predClass[1:5]
 
-confusionMatrix <- table(Dictamen[learn],predClass)
+# confusionMatrix_2 = table(df_wk_i[-learn, 15], predictions)
+confusionMatrix <- table(genre[learn],predClass)
 confusionMatrix
-error_rate.learn <- 100*(confusionMatrix[1,1]+confusionMatrix[2,2])/nlearn 
+error_rate.learn <- 100*sum(diag(confusionMatrix))/nlearn 
 error_rate.learn
 
 
 # ERROR RATE IN THE TEST SAMPLE
 
-tPredictions=predict(tree5,newdata=dtot[-learn,])
-summary(tPredictions)
-dim(tPredictions)
+tpredictions=predict(tree5,newdata=dtot[-learn,])
+summary(tpredictions)
+dim(tpredictions)
 
 tpredClass=NULL
-tpredClass[tPredictions[,1]<0.5]="pred_neg.test"
-tpredClass[tPredictions[,1]>=0.5]="pred_pos.test"
-table(Dictamen[-learn],tpredClass)
-TestConfusionMatrix <- table(Dictamen[-learn],tpredClass)
-error_rate.test <- 100*(TestConfusionMatrix[1,1]+TestConfusionMatrix[2,2])/ntest 
+tpredClass[tpredictions[,1]>=l]="pred_DarkTrap"
+tpredClass[tpredictions[,2]>=l]="pred_dnb"
+tpredClass[tpredictions[,3]>=l]="pred_Emo"
+tpredClass[tpredictions[,4]>=l]="pred_hardstyle"
+tpredClass[tpredictions[,5]>=l]="pred_Hiphop"
+tpredClass[tpredictions[,6]>=l]="pred_Pop"
+tpredClass[tpredictions[,7]>=l]="pred_psytrance"
+tpredClass[tpredictions[,8]>=l]="pred_Rap"
+tpredClass[tpredictions[,9]>=l]="pred_RnB"
+tpredClass[tpredictions[,10]>=l]="pred_techhouse"
+tpredClass[tpredictions[,11]>=l]="pred_trance"
+tpredClass[tpredictions[,12]>=l]="pred_trap"
+tpredClass[tpredictions[,13]>=l]="pred_Trap_Metal"
+tpredClass[tpredictions[,14]>=l]="pred_Underground_Rap"
+table(genre[-learn],tpredClass)
+TestConfusionMatrix <- table(genre[-learn],tpredClass)
+error_rate.test <- 100*sum(diag(TestConfusionMatrix))/ntest 
 error_rate.test
 
+#---------------- ROC Iñigo
+genre.test <- genre[-learn]
+# Obten las clases únicas de genre.test
+unique_classes <- unique(genre.test)
+num_classes <- length(unique_classes)
+
+par("mar")
+par(mar=c(1,1,1,1))
+
+# Preparar un área de trazado con múltiples paneles
+par(mfrow = c(ceiling(length(unique_classes) / 2), 2))
+
+for (class in unique_classes) {
+  # Calcula el número de positivos y negativos para la clase actual
+  npos <- sum(genre.test == class)
+  nneg <- length(genre.test) - npos
+  
+  # Extraer las predicciones para la clase actual
+  pred.test <- tpredictions[, class]
+  rank_pred.test <- rank(pred.test)
+  
+  # Calcular las acumulaciones para falsos positivos y verdaderos positivos
+  binary_class <- ifelse(genre.test == class, "positiu", "negatiu")
+  acum_fals.pos <- 100 * cumsum(rev(as.numeric(tapply(binary_class == "negatiu", rank_pred.test, sum)))) / nneg
+  acum_true.pos <- 100 * cumsum(rev(as.numeric(tapply(binary_class == "positiu", rank_pred.test, sum)))) / npos
+  
+  # Trazar la curva ROC para la clase actual
+  plot(acum_fals.pos, acum_true.pos, type = "l", main = paste("Curva ROC para", class))
+  lines(acum_fals.pos, acum_fals.pos, col = "red")
+}
+
+#----------------
 
 # ROC CURVE
 
-Dict.test <- Dictamen[-learn]
-table(Dict.test)
-npos <- table(Dict.test)[1]
+genre
+genre.test <- genre[-learn]
+table(genre.test)
+npos <- table(genre.test)[1]
 nneg <- ntest - npos
 
 pred.test <- tPredictions[,1]
-
+tpredictions
 # RANKING THE PREDICTIONS
 
+# Cambiar para que en vez de ser positivo y negativo sea multiclase
 rank_pred.test <- rank(pred.test)
 
-acum_fals.pos <- 100*cumsum(rev(as.numeric(tapply(Dict.test=="negatiu",rank_pred.test,sum))))/nneg
+acum_fals.pos <- 100*cumsum(rev(as.numeric(tapply(genre.test=="negatiu",rank_pred.test,sum))))/nneg
 
 # COMPUTING HOW MANY POSITIVE ARE IN EACH LEAVE (LEAVE = EQUAL RANK INDIVIDUALS)
 
-acum_true.pos <- 100*cumsum(rev(as.numeric(tapply(Dict.test=="positiu",rank_pred.test,sum))))/npos
+acum_true.pos <- 100*cumsum(rev(as.numeric(tapply(genre.test=="positiu",rank_pred.test,sum))))/npos
 
 
 # PLOT
@@ -189,14 +246,14 @@ lines(acum_fals.pos, acum_fals.pos, col="red")
 
 sensitivity<-1-acum_fals.pos
 
-acum_fals.neg<- 100*cumsum(rev(as.numeric(tapply(Dict.test=="positiu",rank_pred.test,sum))))/npos
+acum_fals.neg<- 100*cumsum(rev(as.numeric(tapply(genre.test=="positiu",rank_pred.test,sum))))/npos
 plot(acum_fals.neg,acum_fals.pos,type="l", main="ROC curve")
 lines(acum_fals.neg,acum_fals.pos,  col="red")
 
 # OR
 ord_pred.test <- order(pred.test,decreasing=T)
-ac_neg_test <- 100*cumsum(Dict.test[ord_pred.test]=="negatiu")/nneg
-ac_pos_test <- 100*cumsum(Dict.test[ord_pred.test]=="positiu")/npos
+ac_neg_test <- 100*cumsum(genre.test[ord_pred.test]=="negatiu")/nneg
+ac_pos_test <- 100*cumsum(genre.test[ord_pred.test]=="positiu")/npos
 
 plot(ac_neg_test,ac_pos_test,type="l", main="Concentration Curve")
 lines(ac_neg_test, ac_neg_test, col="red")
@@ -217,3 +274,6 @@ ord_pred.test <- order(pred.test,decreasing=T)
 
 plot(ac_tot,ac_pos_test,type="l", main="Concentration Curve")
 lines(ac_tot, ac_tot, col="red")
+
+
+
