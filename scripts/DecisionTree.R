5809d2fc27643a99cf041a5c2c869b8a3f29ae6a
inigopm <inigopm@gmail.com>
2024-01-16 02:08:57 +0100

    Decisino trees finished

--- scripts/DecisionTree.R
+++ scripts/DecisionTree.R
@@ -3,8 +3,10 @@
 library(rpart)
 library(rpart.plot)
 library(tidyverse)
+library(AICcmodavg)
+
 setwd("C:/Users/usuario/Desktop/MVA/spotify_MVA")
-df_wk_i <- readRDS("report/preprocessing.Rdata") #el que tiene el merge
+df_wk_i <- readRDS("../report/preprocessing.Rdata") #el que tiene el merge
 
 dim(df_wk_i)
 names(df_wk_i)
@@ -13,22 +15,31 @@ missingGenre<-which(is.na(df_wk_i[,24]));length(missingGenre) #0
 
 #df_wk_i<-df_wk_i[-missingGenre,]
 
+# df_ <- df_wk_i %>% 
+#   select("Album_type","Danceability","Energy","Key","Loudness",
+#          "Speechiness","Acousticness","Instrumentalness", "Liveness", "Valence",
+#          "Tempo","Duration_ms","scaled_stream", "scaled_views", "genre","mode","time_signature",
+#          "type")
+
 df_ <- df_wk_i %>% 
   select("Album_type","Danceability","Energy","Key","Loudness",
          "Speechiness","Acousticness","Instrumentalness", "Liveness", "Valence",
-         "Tempo","Duration_ms","scaled_stream", "scaled_views", "genre","mode","time_signature",
-         "type")
+         "Tempo","Duration_ms","scaled_stream", "scaled_views", "genre","mode","time_signature")
 
 # Our response variable is genre but our dataset it's unbalanced, so we decided to discriminate by a threshold of 100 observations.
 # In this way we can take only the modalities that are more relevant
 genre_counts <- table(df_$genre)
 selected_genres <- names(genre_counts[genre_counts > 100])
+df_ <- df_[df_$genre %in% selected_genres, ]
+df_$genre <- factor(df_$genre)
 
-df_f <- df_[df_$genre %in% selected_genres, ]
+df_$genre()
 
-attach(df_f)
-sapply(df_f, class)
-summary(df_f)
+levels(df_wk_i$genre)
+
+attach(df_)
+sapply(df_, class)
+summary(df_)
 
 #ASSUMING PREPROCESSED DATA
 # WHICH RESPONSE? -> GENRE
@@ -38,8 +49,10 @@ summary(df_f)
 #declare type of response variable and all factors
 # DECISION TREES   CART
 #by default
+# dtot <- data.frame(Album_type,Danceability,Energy,Key,Loudness,Speechiness,Acousticness,Instrumentalness, Liveness, Valence,
+#                    Tempo,Duration_ms,scaled_stream,scaled_views,genre,mode,time_signature, type)
 dtot <- data.frame(Album_type,Danceability,Energy,Key,Loudness,Speechiness,Acousticness,Instrumentalness, Liveness, Valence,
-                   Tempo,Duration_ms,scaled_stream,scaled_views,genre,mode,time_signature,type)
+                   Tempo,Duration_ms,scaled_stream,scaled_views,genre,mode,time_signature)
 
 #don't predict genre=NA, missing data on the response variable makes no sense
 # HOLDOUT OF A 1/3 OF DATA TO ESTIMATE THE MISCLASSIFICATION PROB. OF THE CHOSEN TREE 
@@ -59,7 +72,7 @@ ntest <- n - nlearn
 
 
 tree1 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
-              Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature+type, data=dtot[learn,])
+              Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,])
 class(tree1)
 
 print(tree1)
@@ -83,14 +96,14 @@ prp(tree1, type=1, extra=4)
 # Analyze control parameters: maxdepth
 
 tree2 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
-                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature+type, data=dtot[learn,], control=rpart.control(maxdepth=3))
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(maxdepth=3))
 
 prp(tree2, type=1, extra= 4)
 print(tree2)
 
 #complexity parameter
 tree3 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
-                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature+type, data=dtot[learn,], control=rpart.control(cp=0.005))
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(cp=0.005))
 
 prp(tree3, type=1, extra=4)
 
@@ -99,20 +112,57 @@ prp(tree3, type=1, extra=4)
 
 
 tree4 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
-                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature+type, data=dtot[learn,], control=rpart.control(cp=0.06))
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(cp=0.02))
 prp(tree4, type=1, extra=4)
 
-#minsplit
+
+
+par(mfrow=c(2,3))
+
+tree4.1 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
+                  Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(cp=0.001))
+prp(tree4.1, type=1,main = paste("cp =", 0.01))
+
+tree4.2 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(cp=0.02))
+prp(tree4.2, type=1,  main = paste("cp =", 0.02))
+
+tree4.3 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(cp=0.03))
+prp(tree4.3, type=1,  main = paste("cp =", 0.03))
+
+tree4.4 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(cp=0.04))
+prp(tree4.4, type=1,  main = paste("cp =", 0.04))
+
+tree4.5 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(cp=0.05))
+prp(tree4.5, type=1, main = paste("cp =", 0.05))
+
+tree4.6 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(cp=0.06))
+prp(tree4.6, type=1,  main = paste("cp =", 0.06))
+
+# Restablecer los parámetros gráficos a su valor por defecto
+par(mfrow=c(1,1))
+
+
+
+
+tree4 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(cp=0.02))
+prp(tree4, type=1, extra=4)#minsplit
 #minimum number of observations required to split an internal node of the tree
 tree5 = rpart(genre~Album_type+Danceability+Energy+Key+Loudness+Speechiness+Acousticness+Instrumentalness+Liveness+Valence+
-                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature+type, data=dtot[learn,], control=rpart.control(minsplit=200))
+                Tempo+Duration_ms+scaled_stream+scaled_views+mode+time_signature, data=dtot[learn,], control=rpart.control(minsplit=200))
 prp(tree5, type=1, extra= 3)
 #this are the basics. Other criteria available, complexity parameter and cross-validation not introduced
 
 
-# CACULATE THE ERROR RATE IN THE LEARNING SAMPLE
+################## CACULATE THE ERROR RATE IN THE LEARNING SAMPLE
 
-predictions=predict(tree5,data=dtot[learn,])
+predictions=predict(tree4,data=dtot[learn,])
+tpredictions=predict(tree4,newdata=dtot[-learn,])
 head(predictions)
 
 # LETS HAVE A LOOK ON THE PREDICTION (conditional probability of LeafLabel)
@@ -127,50 +177,79 @@ nlearn
 dim(dtot[learn,])
 
 table(genre,exclude=FALSE)
-table(df_wk_i[learn,15])
+table(df_[learn,15])
 
 
-table(df_wk_i[,15])
+table(df_[,15])
 names(table(dtot$genre))
 predictions
 # CACULATE THE ERROR RATE IN THE LEARNING SAMPLE
 l<-0.5
 l<-0.3
-# TODO: CAMBARLO A MAXIMO
-predClass=NULL
-predClass[predictions[,1]>=l]="pred_Emo"
-predClass[predictions[,2]>=l]="pred_Hiphop"
-predClass[predictions[,3]>=l]="pred_Pop"
-predClass[predictions[,4]>=l]="pred_Rap"
-predClass[predictions[,5]>=l]="pred_RnB"
-predClass[predictions[,6]>=l]="pred_Underground_Rap"
-predictions[1:5]
-predClass[1:5]
 
-# confusionMatrix_2 = table(df_wk_i[-learn, 15], predictions)
+########### Coge el maximo
+# Calcula la clase con la mayor predicción para cada fila
+predClass <- apply(predictions, 1, function(x) {
+  labels <- c("pred_Emo", "pred_Hiphop", "pred_Pop", "pred_Rap", "pred_RnB", "pred_Underground_Rap")
+  maxLabel <- labels[which.max(x)]
+  return(maxLabel)
+})
+
 confusionMatrix <- table(genre[learn],predClass)
-confusionMatrix
 error_rate.learn <- 100*sum(diag(confusionMatrix))/nlearn 
 error_rate.learn
 
-
 # ERROR RATE IN THE TEST SAMPLE
 
-tpredictions=predict(tree5,newdata=dtot[-learn,])
 summary(tpredictions)
 dim(tpredictions)
 
-tpredClass=NULL
-tpredClass[tpredictions[,1]>=l]="pred_Emo"
-tpredClass[tpredictions[,2]>=l]="pred_Hiphop"
-tpredClass[tpredictions[,3]>=l]="pred_Pop"
-tpredClass[tpredictions[,4]>=l]="pred_Rap"
-tpredClass[tpredictions[,5]>=l]="pred_RnB"
-tpredClass[tpredictions[,6]>=l]="pred_Underground_Rap"
+tpredClass <- apply(tpredictions, 1, function(x) {
+  labels <- c("pred_Emo", "pred_Hiphop", "pred_Pop", "pred_Rap", "pred_RnB", "pred_Underground_Rap")
+  maxLabel <- labels[which.max(x)]
+  return(maxLabel)
+})
 table(genre[-learn],tpredClass)
 TestConfusionMatrix <- table(genre[-learn],tpredClass)
-error_rate.test <- 100*sum(diag(TestConfusionMatrix))/ntest 
-error_rate.test
+error_rate.test <- 100*sum(diag(TestConfusionMatrix))/ntest
+
+######## Validation
+
+print(paste("Error rate train", error_rate.learn))
+print(paste("Error rate test set", error_rate.test))
+
+# Suma de los verdaderos positivos (diagonal principal)
+true_positives <- sum(diag(TestConfusionMatrix))
+
+# Número total de observaciones
+total_observations <- sum(TestConfusionMatrix)
+
+# Calculando el accuracy
+accuracy <- true_positives / total_observations * 100
+accuracy
+
+AIC.rpart <- function(fit) {
+  n <- fit$frame$n[1]  # número de observaciones
+  p <- sum(fit$frame$var != "<leaf>")  # número de parámetros (nodos no terminales)
+  rss <- fit$cptable[which.min(fit$cptable[,"xerror"]),"xerror"]  # suma de cuadrados del error residual
+  aic <- n * log(rss/n) + 2 * p
+  print(n)
+  print(p)
+  print(rss)
+  return(aic)
+}
+
+aic_value <- AIC.rpart(tree1)
+aic_value
+
+aic_value <- AIC.rpart(tree2)
+aic_value
+
+aic_value <- AIC.rpart(tree3)
+aic_value
+
+aic_value <- AIC.rpart(tree4)
+aic_value
 
 #---------------- ROC Iñigo
 genre.test <- genre[-learn]
